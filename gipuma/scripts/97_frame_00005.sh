2102a4e24861ed7db5849a96599c60410859752a
Maram Bayadsi <bayadsi97@gmail.com>
2023-06-02 18:32:54 +0200

    Add script for running existing code for frame05

---
+++ gipuma/scripts/97_frame_00005.sh
@@ -0,0 +1,56 @@
+#!
+prog="./gipuma"
+warping="../fusibile/fusibile"
+inputdir="data/97_frame_00005/"
+batch_name="97_frame_00005_cam"
+output_dir_basename="results/$batch_name"
+p_folder="data/97_frame_00005/cam_par.txt"
+scale=1
+blocksize=11
+iter=8
+cost_gamma=10
+cost_comb="best_n"
+n_best=3
+depth_max=1.7
+depth_min=0.7
+image_list_array=`( cd $inputdir && ls *.png) `
+output_dir=${output_dir_basename}/
+
+# fuse options
+disp_thresh=7
+normal_thresh=70
+num_consistent=3
+min_angle=30
+max_angle=90
+
+#warping conf
+count=0
+for im in $image_list_array
+do
+    echo $count
+    img=${im%.png}
+    cmd_file=${output_dir}/$img-cmd.log
+    image_list=( $im )
+
+    mkdir -p $output_dir
+    for ij in $image_list_array
+    do
+	if [ $im != $ij ]
+	then
+	    image_list+=( $ij )
+	fi
+    done
+    cmd="$prog ${image_list[@]} -images_folder $inputdir -krt_file $p_folder -output_folder $output_dir -no_display --cam_scale=$scale --iterations=$iter --blocksize=$blocksize --cost_gamma=$cost_gamma --cost_comb=best_n --n_best=$n_best --depth_max=$depth_max --depth_min=$depth_min --min_angle=$min_angle --max_angle=$max_angle -color_processing"
+    echo $cmd
+    $cmd
+
+    let "count += 1"
+    if [ $count -eq -1 ]
+    then
+	    break
+    fi
+done
+
+f_cmd="$warping -input_folder $output_dir -krt_file $p_folder -images_folder $inputdir --cam_scale=$scale --depth_min=$depth_min --depth_max=$depth_max --disp_thresh=$disp_thresh --normal_thresh=$normal_thresh --num_consistent=$num_consistent"
+echo $f_cmd
+$f_cmd
